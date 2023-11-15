967aa92f9ef19eeb168de2f214493dafb48c7056
Abhi <aramtel@buffalo.edu>
2023-11-15 18:43:26 -0500

    Added Dark Mode Button to Calendar

--- theta_tau/pages/calender.jsx
+++ theta_tau/pages/calender.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, Dimensions, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  SafeAreaView,
+  Switch,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Navbar from "../components/navbar"
-import Extra from "../components/extra"
-import Event from "../components/event"
-import Data from "../data/data.json"
+import Navbar from '../components/navbar';
+import Extra from '../components/extra';
+import Event from '../components/event';
+import Data from '../data/data.json';
 
 const Brother = (props) => {
   const navigation = useNavigation();
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   // Image sources
   const placeholderImages = [
@@ -20,18 +30,32 @@ const Brother = (props) => {
     require('../assets/bracelet.png'), // DIY BRACELETS
   ];
 
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, isDarkMode && styles.darkMode]}>
       <ScrollView style={styles.scrollView}>
-        <Text style={styles.title}>RUSH EVENTS</Text>
+        <Text style={[styles.title, isDarkMode && styles.darkModeTitle]}>RUSH EVENTS</Text>
+        <View style={styles.darkModeToggle}>
+        <Text style={styles.darkModeText}>Dark Mode</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleDarkMode}
+          value={isDarkMode}
+        />
+      </View>
         <Image style={styles.logo} source={require('../assets/theta_tau_coa.png')} />
-        <Event desc={Data.eventData[0].description} title="Info Session 1" imageSource = {placeholderImages[0]} />
-        <Event desc={Data.eventData[1].description} title="Info Session 2" imageSource = {placeholderImages[1]} />
-        <Event desc={Data.eventData[2].description} title="Game Night" imageSource = {placeholderImages[2]} />
-        <Event desc={Data.eventData[3].description} title="Meet The Brothers" imageSource = {placeholderImages[3]} />
-        <Event desc={Data.eventData[4].description} title="Pizza Night" imageSource = {placeholderImages[4]} />
-        <Event desc={Data.eventData[5].description} title="Paint Night" imageSource = {placeholderImages[5]} />
-        <Event desc={Data.eventData[6].description} title="D.I.Y. Bracelets" imageSource = {placeholderImages[6]} />
+        <Event desc={Data.eventData[0].description} title="Info Session 1" imageSource={placeholderImages[0]} />
+        <Event desc={Data.eventData[1].description} title="Info Session 2" imageSource={placeholderImages[1]} />
+        <Event desc={Data.eventData[2].description} title="Game Night" imageSource={placeholderImages[2]} />
+        <Event desc={Data.eventData[3].description} title="Meet The Brothers" imageSource={placeholderImages[3]} />
+        <Event desc={Data.eventData[4].description} title="Pizza Night" imageSource={placeholderImages[4]} />
+        <Event desc={Data.eventData[5].description} title="Paint Night" imageSource={placeholderImages[5]} />
+        <Event desc={Data.eventData[6].description} title="D.I.Y. Bracelets" imageSource={placeholderImages[6]} />
         <Extra />
       </ScrollView>
       <Navbar style={styles.navbar} />
@@ -44,6 +68,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
+  darkMode: {
+    backgroundColor: '#121212', // Dark mode background color
+  },
   title: {
     height: 100,
     color: '#FFFFFF',
@@ -53,6 +80,9 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     backgroundColor: '#501315',
     padding: 50
+  },
+  darkModeTitle: {
+    backgroundColor: '#1E1E1E', // Dark mode title background color
   },
   logo: {
     width: 30,
@@ -64,6 +94,16 @@ const styles = StyleSheet.create({
   navbar: {
     position: 'absolute',
     bottom: 0,
+  },
+  darkModeToggle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: 16,
+  },
+  darkModeText: {
+    color: '#000000',
+    marginRight: 10,
   },
 });
 
