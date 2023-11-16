7857c9044c02427d4f4c1dd67d9ab61d4619c01c
gary9528 <gauravji@buffalo.edu>
2023-11-15 20:52:13 -0500

    2

--- theta_tau/FirebaseConfig.ts
+++ theta_tau/FirebaseConfig.ts
@@ -20,4 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-export const FIREBAS_DB = getFirestone(FIREBASE_APP);
+export const FIREBASE_DB = getFirestone(FIREBASE_APP);
