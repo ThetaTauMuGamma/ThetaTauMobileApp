5f3b8bb54d2d2e48467c05fc7b5d9702dbded597
Abhi <aramtel@buffalo.edu>
2023-11-16 15:38:55 -0500

    Login Screen

--- theta_tau/pages/login.jsx
+++ theta_tau/pages/login.jsx
@@ -1,10 +1,23 @@
 // login.jsx
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
+import {
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Platform,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
 import { Image } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 Icon.loadFont();
 
@@ -40,10 +53,16 @@ const Login = () => {
   const appStyles = styles(theme);
 
   return (
-    <View style={appStyles.app}>
-      {theme === 'light' ? (
-          <View style={appStyles.loginContainer}>
-            <Text style={[appStyles.title, { color: theme === 'dark' ? '#ffffff' : '#000000' }]}>Login</Text>
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ flex: 1 }}
+      // behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // Adjust this value to move the text input above your keyboard
+      style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={appStyles.app}>
+          {theme === 'light' ? (
+            <View style={appStyles.loginContainer}>
+              {/* ... (rest of your Login component code) */}
+              <Text style={[appStyles.title, { color: theme === 'dark' ? '#ffffff' : '#000000' }]}>Login</Text>
             <Image style={appStyles.logo} source={require('../assets/icon.png')} />
             <View style={appStyles.container}>
               <View style={appStyles.top}>
@@ -52,7 +71,46 @@ const Login = () => {
               <Text style={appStyles.divider}><Text>Or</Text></Text>
               <View style={appStyles.form}>
                 <Text style={appStyles.label}>Email</Text>
-                <TextInput style={appStyles.input} placeholder="Enter your email" keyboardType="email-address" />
+                <TextInput style={appStyles.input} placeholder="Enter your UB mail" keyboardType="email-address" />
+                <Text style={appStyles.label}>Password</Text>
+                <TextInput
+                style={appStyles.input}
+                placeholder="Enter your password"
+                secureTextEntry={Platform.OS === 'ios'}
+                />
+                <View style={appStyles.remember}>
+                  {/* Add your checkbox and text here */}
+                </View>
+                <TouchableOpacity style={appStyles.button}><Text style={appStyles.buttonText}>Log In</Text></TouchableOpacity>
+                <TouchableOpacity style={appStyles.button}><Text style={appStyles.buttonText}>Forgot Password?</Text></TouchableOpacity>
+              </View>
+              <View style={appStyles.bottom}>
+                {/* Add your Forgot Password and Reset Password links here */}
+              </View>
+              {/* <Text style={appStyles.create}>Sign up here</Text> */}
+            </View>
+            <View style={appStyles.themeToggle}>
+              <Text style={[appStyles.themeText, { color: theme === 'dark' ? '#ffffff' : '#000000' }]}>
+                {theme.charAt(0).toUpperCase() + theme.slice(1)} Theme
+              </Text>
+              <TouchableOpacity onPress={switchTheme}>
+                <Icon name={theme === 'dark' ? 'toggle-on' : 'toggle-off'} size={32} color={theme === 'dark' ? '#ffffff' : '#000000'} />
+              </TouchableOpacity>
+              </View>
+            </View>
+          ) : (
+            <View style={appStyles.loginContainer}>
+              {/* ... (rest of your Login component code for dark theme) */}
+              <Text style={[appStyles.title, { color: theme === 'dark' ? '#ffffff' : '#000000' }]}>Login</Text>
+            <Image style={appStyles.logo} source={require('../assets/icon.png')} />
+            <View style={appStyles.container}>
+              <View style={appStyles.top}>
+                {/* Add your social login buttons here */}
+              </View>
+              <Text style={appStyles.divider}><Text>Or</Text></Text>
+              <View style={appStyles.form}>
+                <Text style={appStyles.label}>Email</Text>
+                <TextInput style={appStyles.input} placeholder="Enter your UB mail" keyboardType="email-address" />
                 <Text style={appStyles.label}>Password</Text>
                 <TextInput style={appStyles.input} placeholder="Enter your password" secureTextEntry={true} />
                 <View style={appStyles.remember}>
@@ -74,46 +132,11 @@ const Login = () => {
                 <Icon name={theme === 'dark' ? 'toggle-on' : 'toggle-off'} size={32} color={theme === 'dark' ? '#ffffff' : '#000000'} />
               </TouchableOpacity>
             </View>
-          </View>
-
-      ) : (
-
-          <View style={appStyles.loginContainer}>
-            <Text style={[appStyles.title, { color: theme === 'dark' ? '#ffffff' : '#000000' }]}>Login</Text>
-            <Image style={appStyles.logo} source={require('../assets/icon.png')} />
-            <View style={appStyles.container}>
-              <View style={appStyles.top}>
-                {/* Add your social login buttons here */}
-              </View>
-              <Text style={appStyles.divider}><Text>Or</Text></Text>
-              <View style={appStyles.form}>
-                <Text style={appStyles.label}>Email</Text>
-                <TextInput style={appStyles.input} placeholder="Enter your email" keyboardType="email-address" />
-                <Text style={appStyles.label}>Password</Text>
-                <TextInput style={appStyles.input} placeholder="Enter your password" secureTextEntry={true} />
-                <View style={appStyles.remember}>
-                  {/* Add your checkbox and text here */}
-                </View>
-                <TouchableOpacity style={appStyles.button}><Text style={appStyles.buttonText}>Log In</Text></TouchableOpacity>
-                <TouchableOpacity style={appStyles.button}><Text style={appStyles.buttonText}>Forgot Password?</Text></TouchableOpacity>
-              </View>
-              <View style={appStyles.bottom}>
-                {/* Add your Forgot Password and Reset Password links here */}
-              </View>
-              {/* <Text style={appStyles.create}>Sign up here</Text> */}
             </View>
-            <View style={appStyles.themeToggle}>
-              <Text style={[appStyles.themeText, { color: theme === 'dark' ? '#ffffff' : '#000000' }]}>
-                {theme.charAt(0).toUpperCase() + theme.slice(1)} Theme
-              </Text>
-              <TouchableOpacity onPress={switchTheme}>
-                <Icon name={theme === 'dark' ? 'toggle-on' : 'toggle-off'} size={32} color={theme === 'dark' ? '#ffffff' : '#000000'} />
-              </TouchableOpacity>
-            </View>
-          </View>
-
-      )}
-    </View>
+          )}
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAwareScrollView>
   );
 };
 
